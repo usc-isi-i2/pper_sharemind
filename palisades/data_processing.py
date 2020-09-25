fac825f7b8e29e863f9e97f0b55db5c544c520f5
Tanmay Ghai <tanmay.ghai@workday.com>
2020-09-25 15:40:23 -0700

    redid psi for palisades & implemented jaccard

---
+++ palisades/data_processing.py
@@ -0,0 +1,85 @@
+import csv
+ds1_path = '/Users/tanmay.ghai/Desktop/palisade-development/src/pke/examples/test_data/gen-1k_300-700-5-5-5-zipf-all_200.csv'
+ds2_path = '/Users/tanmay.ghai/Desktop/palisade-development/src/pke/examples/test_data/gen-1k_300-700-5-5-5-zipf-all_800.csv'
+ds1 = {}
+ds2 = {}
+t = 0.5
+N = 10
+
+def jaccard(a, b, t):
+    return (1.0 * (len(a & b) / len(a | b))) >= t
+
+def ngram(n, s, place_holder=' ', padded=False):
+    if len(s) == 0:
+        return []
+    if padded:
+        pad = place_holder * (n - 1)
+        s = pad + s + pad
+    s = s.split(' ')
+    s = place_holder.join(s)
+    if len(s) < n:
+        return [s]
+    return [s[i:i + n] for i in range(len(s) - n + 1)]
+
+
+
+def processing():
+    with open(ds1_path, 'r') as f1:
+        csvfile = csv.DictReader(f1)
+        for idx, line in enumerate(csvfile):
+            if idx >= N:
+                break
+            value = line['culture']\
+                + ' ' + line['sex']\
+                + ' ' + line['age']\
+                + ' ' + line['date_of_birth']\
+                + ' ' + line['title']\
+                + ' ' + line['given_name']\
+                + ' ' + line['surname']\
+                + ' ' + line['state']\
+                + ' ' + line['suburb']\
+                + ' ' + line['postcode']\
+                + ' ' + line['street_number']\
+                + ' ' + line['address_1']\
+                + ' ' + line['address_2']\
+                + ' ' + line['phone_number']\
+                + ' ' + line['soc_sec_id']
+            value = value.lower().strip()
+            value = ngram(2, value)
+            value = list(filter(lambda x: x != '', value))
+            value = [ord(elem) for sub in value for elem in sub]
+            ds1[line['rec_id']] = value
+    with open(ds2_path, 'r') as f2:
+        csvfile = csv.DictReader(f2)
+        for idx, line in enumerate(csvfile):
+            if idx >= N:
+                break
+            value = line['culture']\
+                + ' ' + line['sex']\
+                + ' ' + line['age']\
+                + ' ' + line['date_of_birth']\
+                + ' ' + line['title']\
+                + ' ' + line['given_name']\
+                + ' ' + line['surname']\
+                + ' ' + line['state']\
+                + ' ' + line['suburb']\
+                + ' ' + line['postcode']\
+                + ' ' + line['street_number']\
+                + ' ' + line['address_1']\
+                + ' ' + line['address_2']\
+                + ' ' + line['phone_number']\
+                + ' ' + line['soc_sec_id']
+            value = value.lower().strip()
+            value = ngram(2, value)
+            value = list(filter(lambda x: x != '', value))
+            value = [ord(elem) for sub in value for elem in sub]
+            # value = list(filter(lambda x: x != '__', value))
+            ds2[line['rec_id']] = value
+        with open('../src/pke/examples/test_data/tokenized_set_A.csv', 'w+') as file1:
+            for key in ds1.keys():
+                file1.write("%s:%s\n"%(key, ds1[key]))
+        with open('../src/pke/examples/test_data/tokenized_set_B.csv', 'w+') as file2:
+            for key in ds2.keys():
+                file2.write("%s:%s\n"%(key, ds2[key]))
+
+processing()
--- palisades/psi-palisades.cpp
+++ palisades/psi-palisades.cpp
@@ -28,24 +28,30 @@
 #include <time.h>
 #include <stdlib.h>
 #include "csvstream.h"
+#include <Python.h>
+#include <sstream>
 
 using namespace std;
 using namespace lbcrypto;
 
-vector<vector<string>> readFromCSVFile(string csvFilePath);
-vector<string> tokenHelper(int n, string record);
-deque<pair<string, vector<string>>> tokenizeRecords(vector<vector<string>> set);
-vector<Plaintext> preProcessPlaintexts(CryptoContext<DCRTPoly> cc,
-                                       deque<pair<string, vector<string>>> set);
-vector<int64_t> simpleIntegerPSI(CryptoContext<DCRTPoly> cc,
-                                 LPKeyPair<DCRTPoly> keyPair,
-                                 vector<int64_t> setX, vector<int64_t> setY);
+deque<pair<string, vector<int64_t>>> readFromCSVFile(string csvFilePath);
 
 int main() {
+  Py_Initialize();
+  PyObject *obj =
+      Py_BuildValue("s",
+                    "/Users/tanmay.ghai/Desktop/palisade-development/src/pke/"
+                    "examples/data_processing.py");
+  FILE *file = _Py_fopen_obj(obj, "r+");
+  if (file != NULL) {
+    PyRun_SimpleFile(file, "data_processing.py");
+  }
+  Py_Finalize();
+
   int plaintextModulus = 65537;
   double sigma = 3.2;
   SecurityLevel securityLevel = HEStd_128_classic;
-  uint32_t depth = 2;
+  uint32_t depth = 10;
 
   // Instantiate the BGVrns crypto context
   CryptoContext<DCRTPoly> cc =
@@ -53,9 +59,9 @@ int main() {
           depth, plaintextModulus, securityLevel, sigma, depth, OPTIMIZED, BV);
 
   // Instantiate the BFVrns crypto context
-  //  CryptoContext<DCRTPoly> cc =
-  //      CryptoContextFactory<DCRTPoly>::genCryptoContextBFVrns(
-  //          plaintextModulus, securityLevel, sigma, 0, depth, 0, OPTIMIZED);
+  //    CryptoContext<DCRTPoly> cc =
+  //        CryptoContextFactory<DCRTPoly>::genCryptoContextBFVrns(
+  //            plaintextModulus, securityLevel, sigma, 0, depth, 0, OPTIMIZED);
 
   // Enable features that to use
   cc->Enable(ENCRYPTION);
@@ -69,188 +75,104 @@ int main() {
 
   cc->EvalMultKeyGen(keyPair.secretKey);
 
-    vector<vector<string>> setA = readFromCSVFile(
-        "/Users/tanmay.ghai/Desktop/palisade-development/src/pke/examples/"
-        "test1.csv");
-    vector<vector<string>> setB = readFromCSVFile(
-        "/Users/tanmay.ghai/Desktop/palisade-development/src/pke/examples/"
-        "test2.csv");
-
-    deque<pair<string, vector<string>>> a = tokenizeRecords(setA);
-    deque<pair<string, vector<string>>> b = tokenizeRecords(setB);
+  cout << "reading in files!" << endl;
 
 
-    vector<Plaintext> plaintextsA = preProcessPlaintexts(cc, a);
-    vector<Plaintext> plaintextsB = preProcessPlaintexts(cc, b);
+  deque<pair<string, vector<int64_t>>> setA = readFromCSVFile(
+      "/Users/tanmay.ghai/Desktop/palisade-development/src/pke/examples/"
+      "test_data/tokenized_set_A.csv");
+  deque<pair<string, vector<int64_t>>> setB = readFromCSVFile(
+      "/Users/tanmay.ghai/Desktop/palisade-development/src/pke/examples/"
+      "test_data/tokenized_set_B.csv");
 
-  //  vector<int64_t> setX = {3, 2, 1, 4};
-  //  vector<int64_t> setY = {1, 2, 3, 5};
-  //  cout << "set X: " << setX << endl;
-  //  cout << "set Y: " << setY << endl;
-  //
-  //  cout << "Set Intersection between sets X and Y: "
-  //       << simpleIntegerPSI(cc, keyPair, setX, setY) << endl;
+  vector<Plaintext> plaintextsA;
+  vector<Plaintext> plaintextsB;
 
-    vector<Ciphertext<DCRTPoly>> ciphertexts;
-    for (int i = 0; i < plaintextsB.size(); i++) {
-      Ciphertext<DCRTPoly> ciphertext =
-          cc->Encrypt(keyPair.publicKey, plaintextsB[i]);
-      ciphertexts.push_back(ciphertext);
+  cout << "converting to plaintexts!" << endl;
+
+
+  deque<pair<string, vector<int64_t>>>::iterator it;
+  for (it = setA.begin(); it != setA.end(); it++) {
+    plaintextsA.push_back(cc->MakePackedPlaintext(it->second));
+  }
+
+  deque<pair<string, vector<int64_t>>>::iterator it2;
+  for (it2 = setB.begin(); it2 != setB.end(); it2++) {
+    plaintextsB.push_back(cc->MakePackedPlaintext(it2->second));
+  }
+
+  cout << "encrypting set B!" << endl;
+
+  vector<Ciphertext<DCRTPoly>> ciphertexts;
+  for (int i = 0; i < plaintextsB.size(); i++) {
+    Ciphertext<DCRTPoly> ciphertext =
+        cc->Encrypt(keyPair.publicKey, plaintextsB[i]);
+    ciphertexts.push_back(ciphertext);
+  }
+
+  cout << "running PSI algorithm!" << endl;
+
+  vector<Ciphertext<DCRTPoly>> returnCiphertexts;
+  for (int i = 0; i < ciphertexts.size(); i++) {
+    auto sub1 = cc->EvalSub(ciphertexts[i], plaintextsA[0]);
+    auto sub2 = cc->EvalSub(ciphertexts[i], plaintextsA[1]);
+    auto d = cc->EvalMult(sub1, sub2);
+    for (int j = 2; j < plaintextsA.size(); j++) {
+      d = cc->EvalMult(d, cc->EvalSub(ciphertexts[i], plaintextsA[j]));
     }
+    returnCiphertexts.push_back(d);
+  }
 
-    vector<Ciphertext<DCRTPoly>> returnCiphertexts;
-    for (int i = 0; i < ciphertexts.size(); i++) {
-//      vector<int64_t> v(8192);
-//      generate(v.begin(), v.end(), RandomGenerator(100));
-//      Plaintext r = cc->MakePackedPlaintext(v);
-      auto sub1 = cc->EvalSub(ciphertexts[i], plaintextsA[0]);
-      auto sub2 = cc->EvalSub(ciphertexts[i], plaintextsA[1]);
-      auto d = cc->EvalMult(sub1, sub2);
-      for (int j = 2; j < plaintextsA.size(); j++) {
-        d = cc->EvalMult(d, cc->EvalSub(ciphertexts[i], plaintextsA[j]));
-      }
-      returnCiphertexts.push_back(d);
+  cout << "decrypting and determining set intersection size!" << endl;
+
+  double setIntersectionSize = 0;
+  for (int i = 0; i < returnCiphertexts.size(); i++) {
+    Plaintext decryptResult;
+    cc->Decrypt(keyPair.secretKey, returnCiphertexts[i], &decryptResult);
+    vector<int64_t> v(decryptResult->GetPackedValue().size(), 0);
+    if (decryptResult->GetPackedValue() == v) {
+      setIntersectionSize++;
     }
-
-    vector<string> setIntersectionResult;
-    for (int i = 0; i < returnCiphertexts.size(); i++) {
-      Plaintext decryptResult;
-      cc->Decrypt(keyPair.secretKey, returnCiphertexts[i], &decryptResult);
-      vector<int64_t> v(decryptResult->GetPackedValue().size(), 0);
-      if (decryptResult->GetPackedValue() == v) {
-        setIntersectionResult.push_back(b[i].first);
-      }
-    }
-    cout << "Records in Set Intersection: " << setIntersectionResult << endl;
-
+  }
+  cout << "set intersection size: " << setIntersectionSize << endl;
+  cout << "denominator: " << ((plaintextsA.size() + plaintextsB.size()) - setIntersectionSize) << endl;
+  double jaccard = setIntersectionSize / ((plaintextsA.size() + plaintextsB.size()) - setIntersectionSize);
+  printf("%f\n", jaccard);
   return 0;
 }
 
-vector<int64_t> simpleIntegerPSI(CryptoContext<DCRTPoly> cc,
-                                 LPKeyPair<DCRTPoly> keyPair,
-                                 vector<int64_t> setX, vector<int64_t> setY) {
-  vector<Plaintext> pX;
-  vector<Plaintext> pY;
-
-  // generating simple integer plaintexts for setX
-  for (int i = 0; i < setX.size(); i++) {
-    pX.push_back(cc->MakeIntegerPlaintext(setX[i]));
-  }
-
-  // generating simple integer plaintexts for setY
-  for (int i = 0; i < setY.size(); i++) {
-    pY.push_back(cc->MakeIntegerPlaintext(setY[i]));
-  }
-
-  // encrypting contents of set Y
-  vector<Ciphertext<DCRTPoly>> ciphertexts;
-  for (int i = 0; i < pY.size(); i++) {
-    ciphertexts.push_back(cc->Encrypt(keyPair.publicKey, pY[i]));
-  }
-
-  // implementing Basic PSI protocol from: https://eprint.iacr.org/2017/299.pdf
-  vector<Ciphertext<DCRTPoly>> returnCiphertexts;
-  for (int i = 0; i < ciphertexts.size(); i++) {
-    // sample random non-zero plaintext
-    Plaintext r = cc->MakeIntegerPlaintext((rand() % 100) + 1);
-    // initializing d_i to be product of difference between each c_i and each x
-    // in X
-    auto sub1 = cc->EvalSub(ciphertexts[i], pX[0]);
-    auto sub2 = cc->EvalSub(ciphertexts[i], pX[1]);
-    auto d = cc->EvalMult(sub1, sub2);
-    for (int j = 2; j < pX.size(); j++) {
-      d = cc->EvalMult(d, cc->EvalSub(ciphertexts[i], pX[j]));
-    }
-    returnCiphertexts.push_back(cc->EvalMult(r, d));
-  }
-
-  vector<int64_t> setIntersectionResult;
-  for (int i = 0; i < returnCiphertexts.size(); i++) {
-    Plaintext decryptResult;
-    // decrypting the ciphertexts; if y+i : FHE.decrypt(d_i) = 0, then we know
-    // that y_i exists in X and is part of the set intersection.
-    cc->Decrypt(keyPair.secretKey, returnCiphertexts[i], &decryptResult);
-    if (decryptResult->GetIntegerValue() == 0) {
-      setIntersectionResult.push_back(setY[i]);
-    }
-  }
-
-  return setIntersectionResult;
-}
-
-/*
- * preProcessPlaintexts(map<string, vector<string>>): convert each vector of
- * n-grams into a vector of int64_t's to prepare for encryption with Palisades
- * functions.
- */
-vector<Plaintext> preProcessPlaintexts(CryptoContext<DCRTPoly> cc,
-                                            deque<pair<string, vector<string>>> set) {
-  vector<Plaintext> plaintexts;
-  deque<pair<string, vector<string>>>::iterator it;
-  for (it = set.begin(); it != set.end(); it++) {
-    vector<int64_t> plaintext;
-    vector<string> v = it->second;
-    for (string s : v) {
-      for (char c : s) {
-        plaintext.push_back(int(c));
-      }
-    }
-    plaintexts.push_back(cc->MakePackedPlaintext(plaintext));
-  }
-  return plaintexts;
-}
-
-/*
- * tokenHelper(int n, string record): helper method for the tokenization
- * process, converting a string to a n-gram, with n being a parameter.
- */
-vector<string> tokenHelper(int n, string record) {
-  vector<string> tokenized;
-  if (record.size() == 0) {
-    return tokenized;
-  }
-
-  for (int i = 0; i < record.size() - n + 1; i++) {
-    tokenized.push_back(record.substr(i, n));
-  }
-  return tokenized;
-}
-
-/*
- * tokenizeRecords(vector<vector<string>> set): converts each vector of strings
- * into a tokenized set of n-grams. For my purposes here, I chose bi-grams.
- * ex: ["tanmay"] -> ["ta", "an", "nm", "ma", ay"]
- */
-deque<pair<string, vector<string>>> tokenizeRecords(vector<vector<string>> set) {
-  deque<pair<string, vector<string>>> tokenSet;
-  for (int i = 0; i < set.size(); i++) {
-    string key = set[i][set[i].size() - 1];
-    set[i].erase(set[i].begin());
-    set[i].erase(set[i].end() - 1);
-
-    string s;
-    for (const auto &piece : set[i]) s += piece;
-    tokenSet.push_back(make_pair(key, tokenHelper(2, s)));
-  }
-  return tokenSet;
-}
 /*
  * readFromCSVFile(string csvFilePath): reads in the two csv files, each
  * second-level vector is a single line in the csv file where each string in the
  * vector is a comma separated field in the input.
  */
-vector<vector<string>> readFromCSVFile(string csvFilePath) {
-  csvstream csvin(csvFilePath);
+deque<pair<string, vector<int64_t>>> readFromCSVFile(string csvFilePath) {
+  csvstream csvin(csvFilePath, ':', false);
 
-  vector<vector<string>> records;
+  deque<pair<string, vector<int64_t>>> records;
   vector<pair<string, string>> row;
 
   while (csvin >> row) {
-    vector<string> record;
+    vector<int64_t> record;
+    string key;
     for (unsigned int i = 0; i < row.size(); ++i) {
-      record.push_back(row[i].second);
+      stringstream s_stream(row[i].second);
+      while(s_stream.good()) {
+        string substr;
+        getline(s_stream, substr, ',');
+        if (i == 1) {
+          substr = substr.substr(1);
+        } else if (i == row.size() - 1) {
+          substr = substr.substr(0, row.size() - 2);
+        }
+        if (i == 0) {
+          key = substr;
+        } else {
+          record.push_back(stoi(substr));
+        }
+      }
     }
-    records.push_back(record);
+    records.push_back(make_pair(key, record));
   }
   return records;
 }
